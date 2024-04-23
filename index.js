fde4a5a38f76431368d42160667e0d3a18e1f7ed
g;itchtonny <sirmwangitosh@gmail.com>
2024-04-23 12:04:40 +0300

    fetch lab complete

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch( "https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json();
+  })
+  .then (function(json){
+    return renderBooks(json)
+  })
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
